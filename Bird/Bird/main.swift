94b65622160906e7a1d24cef0401e7ef5f2426cd
Heber Alvarez <heber@Hebers-MacBook-Pro-M3.local>
2026-02-19 22:46:31 -0600

    PokedexUI

---
+++ Bird/Bird/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Bird
+//
+//  Created by Heber Alvarez on 03/04/25.
+//  Copyright © 2025 Ibram Uppal. All rights reserved.
+//
+
+import Foundation
---
+++ PokedexUI/PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by Heber Alvarez on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
